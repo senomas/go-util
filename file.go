2a4daed54adbe4d11c91901c032b6594fbc0aff5
Denmaseno <agus@senomas.com>
2016-04-17 15:14:09 +0700

    fix

--- file.go
+++ file.go
@@ -5,7 +5,7 @@ import "os"
 // RemoveAll directory
 func RemoveAll(path ...string) {
 	var err error
-	for p := range path {
+	for _, p := range path {
 		err = os.RemoveAll(p)
 		Check(err, "Remove error: path '%s'\n%v", p)
 	}
